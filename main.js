f8e80877b8589685c1124cce1f3f91b1d8f906a1
Olladi <aleksandr.zuev01@mail.ru>
2023-09-03 02:10:29 +0300

    Пятый коммит

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+     // дз5
+
+//  задание 1
+
+// let a=0;
+// while(a<=50){
+//     if(a!=0){
+//     console.log(a);}
+//     a++;
+// }
+
+// задание 2
+
+// let a=50;
+// do{
+//     console.log(a);
+//     --a;}
+//     while(a>=25);
+
+
+// задание 3
+
+ for (let a=2500; a<=3000; a++){
+     if(a%2==0)continue;
+     console.log(a);
+ }
